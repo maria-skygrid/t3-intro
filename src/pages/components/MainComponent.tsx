b14e5c89a56e155ee51207acf6b8208657d0d208
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-16 16:59:58 +0900

    Post styles

--- src/pages/components/MainComponent.tsx
+++ src/pages/components/MainComponent.tsx
@@ -6,14 +6,17 @@ const MainComponent = () => {
   
   // Early return
   if(!data) {
-    return <div>Error</div>
+    return <div>Loading...</div>
   }
 
   if (isLoading) {
     return <SyncLoader size={5} color={"#FFCD00"} />
   }
 
-  return data.map((post) => (<div key={post.id}>{post.content}</div>))
+  return data.map((post) => (
+    <div 
+      key={post.id}
+      className="p-8 border-b border-slate-700">{post.content}</div>))
 
 }
 
