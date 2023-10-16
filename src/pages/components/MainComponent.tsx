464243989ce482890d746a444b23eec040ee4fb1
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-16 14:12:19 +0900

    Fix: replace Error with div

--- src/pages/components/MainComponent.tsx
+++ src/pages/components/MainComponent.tsx
@@ -2,14 +2,16 @@ import { api } from "~/utils/api";
 import SyncLoader from "react-spinners/SyncLoader";
 
 const MainComponent = () => {
-  const {data, error, isLoading} = api.posts.getAll.useQuery();
+  const {data, isLoading} = api.posts.getAll.useQuery();
   
   // Early return
-  if(!data) throw new Error(`Fetch failed, no data was found.`)
+  if(!data) {
+    return <div>Error</div>
+  }
 
-  if (isLoading) ( 
-    <SyncLoader size={5} color={"#FFCD00"} />
-  );
+  if (isLoading) {
+    return <SyncLoader size={5} color={"#FFCD00"} />
+  }
 
   return data.map((post) => (<div key={post.id}>{post.content}</div>))
 
