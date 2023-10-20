32b8310d5db1aeed16acccf421084d1f29e1dd1f
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-20 11:51:41 +0900

    Feat: use dayjs to display relative time

--- src/components/molecules/PostMolecule.tsx
+++ src/components/molecules/PostMolecule.tsx
@@ -1,19 +1,27 @@
 import { RouterOutputs } from "~/utils/api";
 import AvatarAtom from "../atoms/AvatarAtom";
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+dayjs.extend(relativeTime)
+
 
 // 上記のtypeと同じですが、APIから取得してるデータで作成みたい。
 type Post = RouterOutputs["posts"]["index"][number];
 
 const PostMolecule = ({post, author}: Post) => {
   return (
-    <div key={post.id}className="p-8 border-b gap-4 border-slate-700 flex items-center">
+    <div key={post.id}className="p-4 border-b gap-4 border-slate-700 flex items-start">
       <AvatarAtom
         src={author.imageUrl} 
         alt="User profile avatar" 
         width={40}
         height={40}
       />
-      <div>{post.content}</div>
+      <div>
+        <span className="font-bold">@{author.username}</span>
+        <span className="text-sm text-slate-400"> · {dayjs().to(dayjs(post.createdAt))}</span>
+        <p>{post.content}</p>
+      </div>
     </div>
   )
 }
