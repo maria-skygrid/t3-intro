8e1ee9d1fe52680bad49cd2a9f03be76178ccba7
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-25 16:48:40 +0900

    Add profile router and endpoint

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,10 @@
 import HeadsBase from "~/components/heads";
-
+import { api } from "~/utils/api";
 const ProfilePage = () => {
+
+  const { data } = api.profile.getUserByUsername.useQuery({username: 'maria-skygrid'})
+  console.log(data);
+  
   return (
     <>
       <HeadsBase />
