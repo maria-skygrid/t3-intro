c63b3a720d2f9da89532c1027a029cdd34171114
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-27 11:08:17 +0900

    Add spacing to components

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -10,7 +10,9 @@ const ProfilePage = () => {
     <>
       <HeadsBase />
       <Layout>
-        <p>Profile view</p>
+        <div className="p-4 border-b gap-4 border-slate-700 flex items-start">
+          <p>{data?.username}</p>
+        </div>
       </Layout>
     </>
   );
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -6,7 +6,9 @@ const SinglePostPage = () => {
     <>
       <HeadsBase />
       <Layout>
-        <p>Post view</p>
+        <div className="p-4 border-b gap-4 border-slate-700 flex items-start">
+          <p>Post view</p>
+        </div>
       </Layout>
     </>
   );
