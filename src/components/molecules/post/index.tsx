c94d55d78e440e0c93cd25445409748482b375d6
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-10-31 11:42:43 +0900

    Feat: display delete button on posts

--- src/components/molecules/post/index.tsx
+++ src/components/molecules/post/index.tsx
@@ -1,12 +1,18 @@
 import { RouterOutputs } from "~/utils/api"
 import AtomsAvatar from "../../atoms/avatar"
 import { relTime } from "~/utils/daysjs"
-
+import { MdDeleteForever } from "react-icons/md"
+import { useUser } from "@clerk/nextjs" 
 
 // 上記のtypeと同じですが、APIから取得してるデータで作成みたい。
 type MoleculesPostType = RouterOutputs["posts"]["index"][number];
 
 const MoleculesPost = ({post, author}: MoleculesPostType) => {
+  const { user } = useUser();
+  console.log(`author id: ${author.id}`);
+  
+  console.log(user?.id);
+  
   return (
     <div key={post.id}className="p-4 border-b gap-4 border-slate-700 flex items-start">
       <AtomsAvatar
@@ -19,6 +25,11 @@ const MoleculesPost = ({post, author}: MoleculesPostType) => {
         <span className="font-bold">@{author.username}</span>
         <span className="text-sm text-slate-400"> · {relTime(post.createdAt)}</span>
         <p>{post.content}</p>
+        {user?.id === author.id && (
+          <div className="text-red-500 hover:cursor-pointer mt-3">
+            <MdDeleteForever />
+          </div>
+         )}
       </div>
     </div>
   )
