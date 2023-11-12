bd21bce01a21a62126bb9e9cd9bc3a9ea0096f9e
maria-skygrid <146316981+maria-skygrid@users.noreply.github.com>
2023-11-12 21:07:15 +0900

    Display selected user posts on profile

---
+++ src/components/organisms/userPosts/index.tsx
@@ -0,0 +1,38 @@
+import { api } from "~/utils/api"
+import MoleculesPost from "~/components/molecules/post";
+
+type UserPostsProps = {
+  userId: string
+}
+
+const UserPosts = ({ userId }: UserPostsProps) => {
+  
+  const {data} = api.posts.index.useQuery()
+  
+  if (!data) return null
+
+  const userPosts = data.filter((posts) => posts.author.id === userId)
+  
+  if (data.length === 0) {
+    return (
+      <div>
+        <p>{userId} has no posts yet.</p>
+      </div>
+    )
+  }
+
+  const renderUserPosts = userPosts.map((posts) => (
+    <div key={posts.post.id}>
+      <MoleculesPost {...posts} />
+    </div>
+  ))
+
+  return (
+    <div className="border-t border-t-slate-700">
+      {}
+      {renderUserPosts}
+    </div>
+  )
+}
+
+export default UserPosts
--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,18 +1,38 @@
 import HeadsBase from "~/components/heads";
 import { api } from "~/utils/api";
+import AtomsAvatar from "~/components/atoms/avatar";
+import UserPosts from "~/components/organisms/userPosts";
+
 const ProfilePage = () => {
 
   const { data } = api.profile.show.useQuery({username: 'maria-skygrid'})
-  
-  // if(!data) {
-  //   throw new Error("Error: Data not found")
-  // }
+  const { data: posts } = api.posts.index.useQuery()
+
+  if(!data) return null;
 
   return (
     <>
       <HeadsBase />
       <div className="p-4 border-b gap-4 border-slate-700 flex items-start">
-        <p>{data?.username}</p>
+        <div>
+          <p>{data.username}</p>
+          <p className="text-sm text-slate-400">
+            {posts ? posts.length : 0} posts
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1>HEADER</h1>
+        <div className='p-5'>
+          <AtomsAvatar 
+            src={data.imageUrl} 
+            alt="profile avatar" 
+            styles="border border-2 border-white"
+            width={130}
+            height={130}
+          />
+        </div>
+        <UserPosts userId={data.id} />
       </div>
     </>
   );
